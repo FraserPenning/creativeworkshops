d5ac91baafc4dd035fa087dde9d592a39696c105
FraserPenning <fraser.penning@gmail.com>
2023-03-11 21:57:18 +1300

    Update docusaurus.config.js

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Creative Workshops',
+  tagline: 'Get that outcome!',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -137,3 +137,6 @@ const config = {
 };
 
 module.exports = config;
+module.exports = {
+  title: 'Creative Workshops',
+};
